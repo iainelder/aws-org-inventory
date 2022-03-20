b7eb0de9773eea86ae62bca86f6480d3e68ffd4b
Iain Samuel McLean Elder <iain@isme.es>
2022-03-20 13:35:02 +0100

    Try Pydantic for validation

--- org_config.py
+++ org_config.py
@@ -1,14 +1,48 @@
-from typing import Any, Dict
+from typing import List
+
+import tomli
+from pydantic import BaseModel, conlist
+
+# Need to use Annotated because of pydantic's incompatibility with mypy.
+# See https://github.com/samuelcolvin/pydantic/issues/156
+from typing_extensions import Annotated
 
 
-class InvalidConfigError(Exception):
+class OrgConfigError(Exception):
     pass
 
 
-class OrgConfig:
-    pass
+class OrgInput(BaseModel):
+    org_name: str
+    management_profile: str
+    regions: Annotated[List[str], conlist(str, min_items=1)]
 
-    def __init__(self, config: Dict[str, Any]) -> None:
-        if config == {}:
-            raise InvalidConfigError(config)
-        pass
+
+class OrgListInput(BaseModel):
+    orgs: Annotated[List[OrgInput], conlist(OrgInput, min_items=1)]
+
+
+def load() -> OrgListInput:
+    with open("orgs.toml", mode="rb") as f:
+        config_dict = tomli.load(f)
+        return OrgListInput(**config_dict)
+
+
+# @dataclass
+# class OrgConfig:
+#     def __init__(self, config: OrgListInput) -> None:
+
+#         if not isinstance(config, dict):
+#             raise OrgConfigError("config is not a dict")
+
+#         try:
+#             orglist = config["orgs"]
+#         except KeyError:
+#             raise OrgConfigError("config has no `orgs` key")
+
+#         if not isinstance(orglist, list):
+#             raise OrgConfigError("`orgs` key is not a list of orgs")
+
+#         for index, org in enumerate(orglist):
+#             if not isinstance(org, dict):
+#                 raise OrgConfigError(f"`orgs` item at index {index} is not a dict")
--- test_org_config.py
+++ test_org_config.py
@@ -1,8 +1,79 @@
+from contextlib import contextmanager
+from typing import Any, ContextManager, Dict, Iterator
+
 import pytest  # type: ignore[import]
+from pytest_mock import MockerFixture
 
-from org_config import InvalidConfigError, OrgConfig
+import org_config
+from org_config import OrgConfigError
 
 
-def test_empty_config() -> None:
-    with pytest.raises(InvalidConfigError):
-        OrgConfig({})
+@contextmanager
+def does_not_raise() -> Iterator[None]:
+    yield
+
+
+def test_load_config_reads_toml(mocker: MockerFixture) -> None:
+
+    mocker.patch(
+        "org_config.open",
+        mocker.mock_open(
+            read_data=(
+                b"[[orgs]]\n"
+                b'org_name = "org1"\n'
+                b'management_profile = "profile1"\n'
+                b'regions = ["eu-west-1"]\n'
+            )
+        ),
+    )
+
+    org_config.load()
+
+    org_config.open.assert_called_once_with("orgs.toml", mode=mocker.ANY)  # type: ignore[attr-defined]
+
+
+# TODO: check errors using Pydantic ValidationError
+
+# @pytest.mark.parametrize(  # type: ignore[misc]
+#     "config,expectation",
+#     [
+#         pytest.param(
+#             None, pytest.raises(OrgConfigError, match="config is not a dict"), id="None"
+#         ),
+#         pytest.param(
+#             [],
+#             pytest.raises(OrgConfigError, match="config is not a dict"),
+#             id="empty list",
+#         ),
+#         pytest.param(
+#             {},
+#             pytest.raises(OrgConfigError, match="config has no `orgs` key"),
+#             id="empty dict",
+#         ),
+#         pytest.param(
+#             {"bread": "butter"},
+#             pytest.raises(OrgConfigError, match="config has no `orgs` key"),
+#             id="some other key",
+#         ),
+#         pytest.param(
+#             {"orgs": "myorg"},
+#             pytest.raises(OrgConfigError, match="`orgs` key is not a list of orgs"),
+#             id="orgs is string",
+#         ),
+#         pytest.param(
+#             {"orgs": ["myorg"]},
+#             pytest.raises(OrgConfigError, match="`orgs` item at index 0 is not a dict"),
+#             id="orgs list item is string",
+#         ),
+#         pytest.param(
+#             {"orgs": [{}]},
+#             pytest.raises(OrgConfigError, match="`orgs` item at index 0 is not a dict"),
+#             id="orgs list item is string",
+#         ),
+#     ],
+# )
+# def test_empty_config(
+#     config: OrgListInput, expectation: ContextManager[OrgConfigError]
+# ) -> None:
+#     with expectation:
+#         OrgConfig(config)
